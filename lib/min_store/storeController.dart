8dc33a2f43c8af5503b821c511c29aad451e5698
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-09 16:19:55 +0100

    Update storeController.dart

--- lib/min_store/storeController.dart
+++ lib/min_store/storeController.dart
@@ -1,4 +1,20 @@
 
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class StoreController extends GetXControllerBuilder{}
+
+// the GetX controller class controls the state of the UI when wrapped an individual widget with its Observer,so that it only rebuilds when there is a change in the state of that particular widget. 
+class StoreController extends GetxController{
+  final storeName = 'Thick Shake'.obs; 
+
+final followerCount = 0.obs; 
+final storeStatus = true.obs; 
+final followerList = [].obs; // list for names of store followers
+final reviews = <StoreReviews>[].obs;  // map for names and their reviews for the store
+
+// text editing controllers
+final storeNameEditingController = TextEditingController(); 
+final reviewEditingController = TextEditingController(); 
+final followerController = TextEditingController(); 
+final reviewNameController = TextEditingController(); 
+}
