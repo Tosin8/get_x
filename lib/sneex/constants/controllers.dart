64ccba06561e5dd2a38e69403800f5c1a729b7ed
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-09 18:20:16 +0100

    Updating Controllers

--- lib/sneex/constants/controllers.dart
+++ lib/sneex/constants/controllers.dart
@@ -1,4 +1,10 @@
 import 'package:get_x/sneex/controllers/authController.dart';
 
+import '../controllers/appController.dart';
+
 AuthController authController = AuthController.instance; 
-//AppController appController = AuthController.instance; 
+AppController appController = AppController.instance; 
+UserController userController = UserController.instance; 
+CartController cartController = CartController.instance; 
+
+PaymentController paymentController = PaymentController.instance; 
--- lib/sneex/screens/authentication/authenticaton.dart
+++ lib/sneex/screens/authentication/authenticaton.dart
@@ -1,3 +1,7 @@
+
+
+import 'package:flutter/material.dart';
+
 class AuthenticationScreen extends StatelessWidget {
   final AppController _appController = Get.find();
 
