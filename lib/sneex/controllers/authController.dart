a3f626c23eb47b36d6bf4c8923292f3615cca7fd
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-09 16:20:04 +0100

    Update authController.dart

--- lib/sneex/controllers/authController.dart
+++ lib/sneex/controllers/authController.dart
@@ -2,4 +2,11 @@ import 'package:get/get.dart';
 
 class AuthController extends GetxController{
   static AuthController instance = Get.find();
+  RxBool isLoggedIn = false.obs; 
+
+  @override
+  void onReady(){
+    super.onReady();
+    _setInitialScreen(); 
+  }
 }
