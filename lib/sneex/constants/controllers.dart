37764d8795287536b449a64b5f95aa39f5ae98f7
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-11 17:43:45 +0100

    adding controllers

--- lib/sneex/constants/controllers.dart
+++ lib/sneex/constants/controllers.dart
@@ -1,10 +1,12 @@
 import 'package:get_x/sneex/controllers/authController.dart';
 
 import '../controllers/appController.dart';
+import '../controllers/cart_controller.dart';
+import '../controllers/payment_controller.dart';
 
 AuthController authController = AuthController.instance; 
 AppController appController = AppController.instance; 
-// UserController userController = UserController.instance; 
-// CartController cartController = CartController.instance; 
+ //UserController userController = UserController.instance; 
+ CartController cartController = CartController.instance; 
 
-// PaymentController paymentController = PaymentController.instance; 
+ PaymentsController paymentsController = PaymentsController.instance; 
