70e9044b9fa45173b2998b4e55b5460e2f5c6675
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-14 20:01:39 +0100

    Updating Image

--- lib/simple_cart/model/product_model.dart
+++ lib/simple_cart/model/product_model.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class Product {
   String name; 
-  String price; 
+  double price; 
   String imageUrl; 
   Product({
     required this.name,
@@ -9,3 +9,23 @@ class Product {
     required this.imageUrl,
   });
 }
+
+
+List<Product> products = [
+  Product(
+    name: 'Food',
+     price: 2.99, 
+     imageUrl: 'assets/image/1.png'), 
+       Product(
+    name: 'Apple',
+     price: 1.99, 
+     imageUrl: 'assets/image/2.png'), 
+       Product(
+    name: 'Grape',
+     price:6.11, 
+     imageUrl: 'assets/image/3.png'), 
+       Product(
+    name: 'Tomato',
+     price: 2.99, 
+     imageUrl: 'assets/image/4.png'), 
+]; 
