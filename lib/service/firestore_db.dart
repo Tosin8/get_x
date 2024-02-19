3b254467f8e5fa3edb4ef6b579603905e44bf1bd
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-19 15:03:55 +0100

    Update firestore_db.dart

--- lib/service/firestore_db.dart
+++ lib/service/firestore_db.dart
@@ -1,7 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../simple_cart/model/product_model.dart';
+
 class FirestoreDB {
   // initialize firebase cloud firestore. 
 
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+
+Stream<List<Product>> getAllProducts() {
+  return _firebaseFirestore
+  .collection('products')
+  .snapshots() 
+  .map((snapshot) {
+    return snapshot.docs.map((doc) => Product.fromSnapshot(doc)).toList(); 
+  }); 
+}
 }
