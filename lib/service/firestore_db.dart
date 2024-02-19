70a8856bf3678fe0fc3eeac9ad29d83dff0000cd
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-19 14:50:28 +0100

    Create firestore_db.dart

---
+++ lib/service/firestore_db.dart
@@ -0,0 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreDB {
+  // initialize firebase cloud firestore. 
+
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+}
