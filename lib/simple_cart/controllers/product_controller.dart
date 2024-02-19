d4ba8fa68ad0e334abfca60b34e0236f434723c3
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-19 15:06:35 +0100

    Create product_controller.dart

---
+++ lib/simple_cart/controllers/product_controller.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+import '../model/product_model.dart';
+
+// connecting it to database. 
+class ProductController extends GetxController{
+  // add a list of products objects. 
+
+  final products = <Product>[].obs;
+}
